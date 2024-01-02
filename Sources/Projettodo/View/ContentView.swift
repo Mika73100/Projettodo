aef88559aa9e61d04ae7ea75a83053d6212e4d32
Michael BARRECA <96046263+Mika73100@users.noreply.github.com>
2024-01-02 22:48:37 +0100

    To-do

---
+++ Sources/Projettodo/View/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  SwiftUI Simple TodoList
+//
+//  Created by Rizal Hilman on 20/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: user typed keyword
+    @State var searchKeyword: String = ""
+
+    var results: [Todo] {
+        return searchKeyword.isEmpty ? todos : todos.filter({ todo in
+            todo.title.lowercased().contains(searchKeyword.lowercased())
+        })
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(results, id: \.self) { todo in
+                    NavigationLink(destination: TodoDetailView(todo: todo)) {
+                        HStack(alignment: .center) {
+                            VStack(alignment: .leading) {
+                                Text(todo.title)
+                                    .font(.title3)
+                                Text(formatDate(todo.date))
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            Spacer()
+                            StatusIndicator(status: todo.status)
+                        }
+                    }
+                }
+            }
+            .listStyle(.inset)
+            .padding()
+            .navigationTitle("Todo List")
+            // MARK: Add searchable modifier
+            .searchable(text: $searchKeyword)
+        }
+    }
+
+    private func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter.string(from: date)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tests/ProjettodoTests/ProjettodoTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Projettodo
+
+final class ProjettodoTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
